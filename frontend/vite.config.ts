1ca3d05ca710083b14bde23b3176f019bf3184f1
agodse21 <agodse9@gmail.com>
2024-10-06 23:52:52 +0530

    feat: :sparkles: Added shad cn componets and used

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import { viteStaticCopy } from "vite-plugin-static-copy";
 import { resolve } from "path";
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -20,11 +21,17 @@ export default defineConfig({
   server: {
     open: "/popup-local.html",
   },
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
+    },
+  },
   build: {
     rollupOptions: {
       input: {
         popup: resolve(__dirname, "popup.html"),
         options: resolve(__dirname, "options.html"),
+        background: 'src/chrome-extension/background/background.ts',
       },
       output: {
         entryFileNames: "[name].js",
